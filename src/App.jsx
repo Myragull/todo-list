ffc9899f3d9a16ed1e6a02d307a1578342beb941
Myra Gull <myragull749@gmail.com>
2025-05-26 09:38:46 +0500

    added the css for the line through and laso wraped the li elemnts like checkbox,text and button a div for layouting

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   const [title, settitle] = useState("");
   const [todos, settodos] = useState([]);
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const newtodo = {
@@ -17,48 +17,56 @@ function App() {
     settitle("");
   };
 
-const handleToggle = (id) => {
-  const updatedTodos = todos.map((todo) =>
-    todo.id === id ? { ...todo, completed: !todo.completed } : todo
-  );
-  settodos(updatedTodos);
-};
+  const handleToggle = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    settodos(updatedTodos);
+  };
 
-
- const handlerDelete = (id) => {
+  const handlerDelete = (id) => {
     const filteredTodos = todos.filter((todo) => todo.id !== id);
     settodos(filteredTodos);
   };
- 
+
   return (
     <div className="main-container">
       <div className="content-container">
-      <h1>To-Do List</h1>
+        <h1>To-Do List</h1>
         <form onSubmit={handleSubmit}>
           <input
-          className="user-input"
+            className="user-input"
             type="text"
             value={title}
             placeholder="Add a new task"
             onChange={(e) => settitle(e.target.value)}
           />
-          <button className="add-btn" type="submit">Add</button>
+          <button className="add-btn" type="submit">
+            Add
+          </button>
         </form>
         {
           <ul className="todo-container">
             {todos.map((todo) => {
               return (
-                <li
-                className="list-li"
-                key={todo.id}>
+                <li className="list-li" key={todo.id}>
+                  <div className="todo-item">
                   <label>
-          <input type="checkbox" 
-          checked={todo.completed}
-          onChange={()=>handleToggle(todo.id)}
-          />
-           <span className="todo-text">{todo.title}</span>
-         </label>
-         
+                    <input
+                      type="checkbox"
+                      checked={todo.completed}
+                      onChange={() => handleToggle(todo.id)}
+                    />
+                    <span
+                      className={
+                        todo.completed ? "todo-text completed" : "todo-text"
+                      }
+                    >
+                      {todo.title}
+                    </span>
+                  </label>
+                  </div>
+
                   <button
                     className="delete-button"
                     onClick={() => handlerDelete(todo.id)}
