b06bb306b41edc29230c4eb923d1760d167a7fb5
Myra Gull <myragull749@gmail.com>
2025-05-30 16:25:11 +0500

    Format the code

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,43 +1,42 @@
 import { nanoid } from "nanoid";
-import {useForm} from "react-hook-form"
+import { useForm } from "react-hook-form";
 
-function Form({todos,settodos}) {
-
+function Form({ todos, settodos }) {
   const {
-  register,
-  handleSubmit,
-  reset,
-  formState: { errors },
-} = useForm();
-   
-const onSubmit=(data)=>{
-  if(data.title.trim() === ""){
-    return;
-  }
-  const newtodo = {
-    id: nanoid(),
-    title: data.title,
-    completed: false,
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    if (data.title.trim() === "") {
+      return;
+    }
+    const newtodo = {
+      id: nanoid(),
+      title: data.title,
+      completed: false,
+    };
+    settodos([...todos, newtodo]);
+    reset();
   };
-  settodos([...todos, newtodo]);
-  reset();
-}
-  
+
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            className="user-input"
-            type="text"
-            placeholder="Add a new task"
-            {...register("title")}
-          />
-          <button className="add-btn" type="submit">
-            Add
-          </button>
-        </form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className="user-input"
+          type="text"
+          placeholder="Add a new task"
+          {...register("title")}
+        />
+        <button className="add-btn" type="submit">
+          Add
+        </button>
+      </form>
     </>
-  )
+  );
 }
 
-export default Form
+export default Form;
